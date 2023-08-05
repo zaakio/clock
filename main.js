b2feb056b87efc5bdb75e7d99c8a79e675402d2d
zaakio <140279814+zaakio@users.noreply.github.com>
2023-08-05 23:52:23 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,37 @@
+let [seconds,minutes,hours] = [0,0,0];
+let display = document.getElementById('display');
+let timer = null
+
+function stopwatch() {
+    seconds++;
+    if(seconds == 60){
+        seconds = 0;
+        minutes++;
+        if(minutes== 60){
+            minutes = 0;
+            hours++;
+        }
+    }
+
+    let h = hours < 10 ? "0" + hours : hours;
+    let m = minutes < 10 ? "0" + minutes : minutes;
+    let s = seconds < 10 ? "0" + seconds : seconds;
+
+    display.innerHTML = h +":"+m+":"+s;
+}
+function watchstart(){
+    if(timer !== null){
+        clearInterval(timer);
+    }
+    timer = setInterval(stopwatch,1000);
+}
+
+
+function watchstop(){
+    clearInterval(timer);
+}
+function watchreset(){
+    clearInterval(timer);
+    [seconds,minutes,hours] = [0,0,0];
+    display.innerHTML = "00:00:00"
+}
